80cd8e8358074d472929c91c2b7d26430a403674
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2021-03-18 15:42:13 +0100

    First Lesson

---
+++ _PRIMA/L01_FirstFudge/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var L01_FirstFudge;
+(function (L01_FirstFudge) {
+    var f = FudgeCore;
+    let node = new f.Node("firstNode");
+    console.log(node);
+})(L01_FirstFudge || (L01_FirstFudge = {}));
+//# sourceMappingURL=index.js.map
---
+++ _PRIMA/L01_FirstFudge/index.ts
@@ -0,0 +1,5 @@
+namespace L01_FirstFudge {
+    import f = FudgeCore;
+    let node: f.Node = new f.Node("firstNode");
+    console.log(node);
+}
