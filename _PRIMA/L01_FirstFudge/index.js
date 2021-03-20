4918639b55a6bd6a8a283ef64ebcd9befd369f85
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2021-03-20 12:18:55 +0100

    finishing L01

--- _PRIMA/L01_FirstFudge/index.js
+++ _PRIMA/L01_FirstFudge/index.js
@@ -2,7 +2,26 @@
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
     var f = FudgeCore;
-    let node = new f.Node("firstNode");
-    console.log(node);
+    // let node: f.Node = new f.Node("firstNode");
+    // console.log(node);
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        const canvas = document.querySelector("canvas");
+        f.Debug.log(canvas);
+        let node = new f.Node("Quad");
+        let mesh = new f.MeshQuad();
+        let componentMesh = new f.ComponentMesh(mesh);
+        node.addComponent(componentMesh);
+        let materialSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let componentMaterial = new f.ComponentMaterial(materialSolidWhite);
+        node.addComponent(componentMaterial);
+        let componentCamera = new f.ComponentCamera();
+        componentCamera.mtxPivot.translateZ(2);
+        componentCamera.mtxPivot.rotateY(180);
+        L01_FirstFudge.viewport = new f.Viewport();
+        L01_FirstFudge.viewport.initialize("Viewport", node, componentCamera, canvas);
+        f.Debug.log(L01_FirstFudge.viewport);
+        L01_FirstFudge.viewport.draw();
+    }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=index.js.map
--- _PRIMA/L01_FirstFudge/index.ts
+++ _PRIMA/L01_FirstFudge/index.ts
@@ -1,5 +1,33 @@
 namespace L01_FirstFudge {
     import f = FudgeCore;
-    let node: f.Node = new f.Node("firstNode");
-    console.log(node);
+    // let node: f.Node = new f.Node("firstNode");
+    // console.log(node);
+    window.addEventListener("load", handleLoad);
+    export let viewport: f.Viewport;
+
+    function handleLoad(_event: Event): void {
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        f.Debug.log(canvas);
+
+        let node: f.Node = new f.Node("Quad");
+
+        let mesh: f.MeshQuad = new f.MeshQuad();
+        let componentMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
+        node.addComponent(componentMesh);
+
+        let materialSolidWhite: f.Material = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let componentMaterial: f.ComponentMaterial = new f.ComponentMaterial(materialSolidWhite);
+        node.addComponent(componentMaterial);
+
+        let componentCamera: f.ComponentCamera = new f.ComponentCamera();
+        componentCamera.mtxPivot.translateZ(2);
+        componentCamera.mtxPivot.rotateY(180);
+
+        viewport = new f.Viewport();
+        viewport.initialize("Viewport", node, componentCamera, canvas);
+
+        f.Debug.log(viewport);
+
+        viewport.draw();
+    }
 }
