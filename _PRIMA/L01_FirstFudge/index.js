26a57877f6bfffc4809f2da55da02d83fe1c6b51
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2021-03-25 17:22:21 +0100

    finished FirstFudge

--- _PRIMA/L01_FirstFudge/index.js
+++ _PRIMA/L01_FirstFudge/index.js
@@ -5,23 +5,39 @@ var L01_FirstFudge;
     // let node: f.Node = new f.Node("firstNode");
     // console.log(node);
     window.addEventListener("load", handleLoad);
+    let viewport;
+    let node = new f.Node("Quad");
+    let fps = 60;
     function handleLoad(_event) {
         const canvas = document.querySelector("canvas");
         f.Debug.log(canvas);
-        let node = new f.Node("Quad");
-        let mesh = new f.MeshQuad();
+        node.addComponent(new f.ComponentTransform());
+        // let node: f.Node = new f.Node("Quad");
+        let mesh = new f.MeshCube;
+        console.log(mesh);
         let componentMesh = new f.ComponentMesh(mesh);
+        // componentMesh.mtxPivot.translateY(-0.5);
         node.addComponent(componentMesh);
-        let materialSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let materialSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("YELLOW")));
         let componentMaterial = new f.ComponentMaterial(materialSolidWhite);
         node.addComponent(componentMaterial);
         let componentCamera = new f.ComponentCamera();
-        componentCamera.mtxPivot.translateZ(2);
-        componentCamera.mtxPivot.rotateY(180);
-        L01_FirstFudge.viewport = new f.Viewport();
-        L01_FirstFudge.viewport.initialize("Viewport", node, componentCamera, canvas);
-        f.Debug.log(L01_FirstFudge.viewport);
-        L01_FirstFudge.viewport.draw();
+        componentCamera.mtxPivot.translateZ(-2);
+        // componentCamera.mtxPivot.rotateY(180);
+        console.log("Node:", node);
+        console.log("Cam: ", componentCamera);
+        viewport = new f.Viewport();
+        viewport.initialize("Viewport", node, componentCamera, canvas);
+        f.Debug.log(viewport);
+        viewport.draw();
+        f.Loop.start(f.LOOP_MODE.TIME_REAL, fps);
+        f.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+    }
+    function update(_event) {
+        let rootSpeed = 90;
+        let timeSinceLastFrameInSeconds = f.Loop.timeFrameReal / 100;
+        node.getComponent(f.ComponentMesh).mtxPivot.rotateY(timeSinceLastFrameInSeconds * rootSpeed);
+        viewport.draw();
     }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=index.js.map
--- _PRIMA/L01_FirstFudge/index.ts
+++ _PRIMA/L01_FirstFudge/index.ts
@@ -3,31 +3,51 @@ namespace L01_FirstFudge {
     // let node: f.Node = new f.Node("firstNode");
     // console.log(node);
     window.addEventListener("load", handleLoad);
-    export let viewport: f.Viewport;
-
+    let viewport: f.Viewport;
+    let node: f.Node = new f.Node("Quad");
+    let fps: number = 60;
     function handleLoad(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
         f.Debug.log(canvas);
 
-        let node: f.Node = new f.Node("Quad");
+        node.addComponent(new f.ComponentTransform());
 
-        let mesh: f.MeshQuad = new f.MeshQuad();
+
+
+        // let node: f.Node = new f.Node("Quad");
+
+        let mesh: f.Mesh = new f.MeshCube;
+        console.log(mesh);
         let componentMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
+        // componentMesh.mtxPivot.translateY(-0.5);
         node.addComponent(componentMesh);
 
-        let materialSolidWhite: f.Material = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let materialSolidWhite: f.Material = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("YELLOW")));
         let componentMaterial: f.ComponentMaterial = new f.ComponentMaterial(materialSolidWhite);
         node.addComponent(componentMaterial);
 
         let componentCamera: f.ComponentCamera = new f.ComponentCamera();
-        componentCamera.mtxPivot.translateZ(2);
-        componentCamera.mtxPivot.rotateY(180);
+        componentCamera.mtxPivot.translateZ(-2);
+        // componentCamera.mtxPivot.rotateY(180);
+
+        console.log("Node:", node);
+        console.log("Cam: ", componentCamera);
 
         viewport = new f.Viewport();
         viewport.initialize("Viewport", node, componentCamera, canvas);
 
         f.Debug.log(viewport);
 
+        viewport.draw();
+
+
+        f.Loop.start(f.LOOP_MODE.TIME_REAL, fps);
+        f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
+    }
+    function update(_event: Event): void {
+        let rootSpeed: number = 90;
+        let timeSinceLastFrameInSeconds: number = f.Loop.timeFrameReal / 100;
+        node.getComponent(f.ComponentMesh).mtxPivot.rotateY(timeSinceLastFrameInSeconds * rootSpeed);
         viewport.draw();
     }
 }
